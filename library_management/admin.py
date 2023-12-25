a6388bdc3aa5a606d20536debe24a87370ba3ed9
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-12-25 16:35:00 +0100

    Implemented models and registered on admin panel

--- library_management/admin.py
+++ library_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from library_management.models import (
+    Book,
+    Borrowing,
+    Payment
+)
+
+
+admin.site.register(Book)
+admin.site.register(Borrowing)
+admin.site.register(Payment)
--- library_management/models.py
+++ library_management/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from library_service import settings
+
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4, choices=CoverChoices.choices, null=True, blank=True
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField()
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowing"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+
+    status = models.CharField(max_length=7, choices=StatusChoices.choices)
+    type = models.CharField(max_length=7, choices=TypeChoices.choices)
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payment"
+    )
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField()
